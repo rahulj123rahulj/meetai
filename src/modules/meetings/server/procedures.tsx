f3bd3097071774a99212b933d645a13e10f69f84
rahul <4r7h2000rj@gmail.com>
2026-02-13 11:05:55 +0530

    update

--- src/modules/meetings/server/procedures.tsx
+++ src/modules/meetings/server/procedures.tsx
@@ -1,6 +1,6 @@
 import { db } from '@/db';
 import { agents, meetings, user } from '@/db/schema';
-import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
+import { createTRPCRouter, premiumProcedure, protectedProcedure } from '@/trpc/init';
 import { and, count, desc, eq, getTableColumns, ilike, inArray, sql } from 'drizzle-orm';
 import z from 'zod';
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from '@/constants';
@@ -112,7 +112,7 @@ export const meetingsRouter = createTRPCRouter({
         });
         return token;
     }),
-    create: protectedProcedure
+    create: premiumProcedure("meetings")
         .input(MeetingInsertSchema)
         .mutation(async ({ input, ctx }) => {
             const [createdMeeting] = await db
